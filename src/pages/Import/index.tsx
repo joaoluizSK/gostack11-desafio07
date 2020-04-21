ecbb7d16e968557ea67e6ffa56b2428b6eff0611
Joao Kestering <jlsk25@gmail.com>
2020-04-21 20:22:02 -0300

    Desafio 7

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 
 import filesize from 'filesize';
 
@@ -20,22 +19,29 @@ interface FileProps {
 
 const Import: React.FC = () => {
   const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
-  const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    data.append('file', uploadedFiles[0].name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    setUploadedFiles(
+      files.map((file) => {
+        return {
+          file,
+          name: file.name,
+          readableSize: filesize(file.size),
+        };
+      }),
+    );
   }
 
   return (
